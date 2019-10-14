d36f2fb98cd994769c0f31a587a03998bbba0be4
tanduong <tanduong@live.com>
2019-10-14 11:51:00 -0500

    updates

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,10 +3,16 @@ import shutil
 
 project_folder = './frontend'
 pm_option = '{{cookiecutter.pm_option}}'
+js_option = '{{cookiecutter.js_option}}'
 
-#'upload/.yarnrc'
 if os.path.isfile('{{cookiecutter.pm_config_file}}'):
     if pm_option == "npm":
         shutil.copy2('{{cookiecutter.pm_config_file}}', project_folder + "/.npmrc")
     if pm_option == "yarn":
         shutil.copy2('{{cookiecutter.pm_config_file}}', project_folder + "/.yarnrc")
+
+if os.path.isfile('{{cookiecutter.js_config_file}}'):
+    if js_option == "flow":
+        shutil.copy2('{{cookiecutter.js_config_file}}', project_folder + "/.flowconfig")
+    if js_option == "typescript":
+        shutil.copy2('{{cookiecutter.js_config_file}}', project_folder + "/tsconfig.json")
