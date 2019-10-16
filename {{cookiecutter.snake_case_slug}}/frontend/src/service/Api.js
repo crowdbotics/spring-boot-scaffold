34a292ec29fb6f68629ddc85535bdb4b82367a6c
tanduong <tanduong@live.com>
2019-10-16 17:55:21 -0500

    update

--- {{cookiecutter.snake_case_slug}}/frontend/src/service/Api.js
+++ {{cookiecutter.snake_case_slug}}/frontend/src/service/Api.js
@@ -3,7 +3,23 @@ import { appConfig } from '../constant'
 export const fetchLogin = async (email, password) => {
   try {
     const { apiKey, apiUrl } = appConfig
-    const url = `${apiUrl}\login?key=${apiKey}`
+    const url = `${apiUrl}\login` //no apiKey for now
+
+    const data = {
+      "emailAddress": email,
+      "password" : password,
+    }
+
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data), 
+    });
+
+    const json = response.json(); 
 
     return true
 
@@ -15,7 +31,26 @@ export const fetchLogin = async (email, password) => {
 export const fetchRegister = async (email, password, fname, lname) => {
   try {
     const { apiKey, apiUrl } = appConfig
-    const url = `${apiUrl}\register?key=${apiKey}`
+    const url = `${apiUrl}\user\register` //no apiKey for now
+
+    const data = {
+      "firstName" : fname,
+      "lastName" : lname,
+      "password" : password,
+      "matchingPassword" : password,
+      "emailAddress": email,
+    }
+
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data), 
+    });
+
+    const json = response.json(); 
 
     return true
 
