3a46d057c6a9f82792fd4cbd0c3c48a5863b9b78
Charath Ranganathan <charath@pfactor.io>
2019-10-18 19:01:26 -0700

    Modified Application.java to fix compilation issues

--- {{cookiecutter.snake_case_slug}}/backend/src/main/java/{{cookiecutter.top_level_domain_name}}/{{cookiecutter.second_level_domain_name}}/{{cookiecutter.project_package_name}}/Application.java
+++ {{cookiecutter.snake_case_slug}}/backend/src/main/java/{{cookiecutter.top_level_domain_name}}/{{cookiecutter.second_level_domain_name}}/{{cookiecutter.project_package_name}}/Application.java
@@ -2,6 +2,10 @@ package {{cookiecutter.top_level_domain_name}}.{{cookiecutter.second_level_domai
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+{%- if cookiecutter.spring_boot_authentication == "jwt" %}
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+{%- endif %}
 
 /**
  * <h1>Application</h1>
@@ -12,6 +16,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
+
+{%- if cookiecutter.spring_boot_authentication == "jwt" %}
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+{%- endif %}
 
 	public static void main( final String [] args ) {
 		SpringApplication.run( Application.class, args );
